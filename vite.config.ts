45a9ef48638e369114af5fa0fb7115a0b870f2b9
hello-itss-me <melkeeee46@gmail.com>
2024-12-28 23:46:58 +0300

    Initial commit from your app

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
 import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  server: {
-    proxy: {
-      '/.netlify/functions': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
+    import react from '@vitejs/plugin-react';
+    import { defineConfig } from 'vite';
+    
+    export default defineConfig({
+      plugins: [react()],
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './src'),
+        },
       },
-    },
-  },
-});
+      optimizeDeps: {
+        exclude: ['lucide-react'],
+      },
+      server: {
+        proxy: {
+          '/.netlify/functions': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+          },
+        },
+      },
+      build: {
+        outDir: 'dist',
+      },
+    });
